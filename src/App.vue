62fb820e33c72692aceb940006def2f30bf53b36
BetterCallAle <dap22091998@gmail.com>
2022-11-10 16:49:57 +0100

    added a loading screen

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,13 @@ export default{
         }
     },
     created() {
+        // add a flag for a loading animation
+        this.store.isLoaded = false;
+        // export the data api in store.characters
         axios.get("https://www.breakingbadapi.com/api/characters").then((resp) => {
             this.store.characters = resp.data;
-            console.log(this.store);
+            // turn off the loading animation
+            this.store.isLoaded = true;
         })
     }
   
@@ -33,11 +37,32 @@ export default{
 
     <main>
         <AppSelector />
-        <AppMain />
+        <!-- Loading -->
+        <div class="loading text-center" v-if="!store.isLoaded">
+            <img src="../src/assets/img/loading.png" alt="Walter White Loading Meme" class="loading-img">
+            <span class="loading-txt text-light d-block">Loading...</span>
+        </div>
+        <!-- Main after the loading screen -->
+        <AppMain v-else />
     </main>
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
 
+.loading-img {
+    width: 300px;
+    margin: 4rem 0;
+    animation: rotating 1s infinite;
+}
+
+@keyframes rotating {
+    from {
+        transform: rotate(0deg);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
+}
 </style>
