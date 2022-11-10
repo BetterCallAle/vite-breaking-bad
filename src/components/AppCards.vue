d25ce2ed03dd7555d58c71bad7b64a7d61d66f31
BetterCallAle <dap22091998@gmail.com>
2022-11-10 16:36:36 +0100

    the cards are now dynamic

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -9,15 +9,15 @@ export default {
 <template>
     <div class="ms_card">
         <div class="ms_card-top">
-            <img src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg"
-                alt="">
+            <img :src="character.img"
+                :alt="`Foto di ${character.name}`">
         </div>
 
         <div class="ms_card-bottom text-center">
-            <h3 class="ms_card-title text-light">Nome</h3>
+            <h3 class="ms_card-title text-light">{{ character.name }}</h3>
 
-            <h4 class="ms_card-subtitle ">Serie</h4>
-            <p class="ms_card-subtitle">Morto</p>
+            <h4 class="ms_card-subtitle ">{{ character.category }}</h4>
+            <p class="ms_card-subtitle">{{ character.status }}</p>
         </div>
     </div>
 </template>
@@ -28,15 +28,20 @@ export default {
 .ms_card {
     background-color: $main-bg;
     padding: 1rem .8rem;
-
+    min-height: 380px;
     .ms_card-bottom {
-        .ms_card-title {
-            font-size: 1.3rem;
-            margin-top: 1rem
-        }
-
-        h4 {
-            font-size: 1.1rem;
+            .ms_card-title {
+                font-size: 1rem;
+                    text-transform: uppercase;
+                    margin-top: 1rem
+                }
+                
+                h4 {
+                    font-size: .9rem;
+                }
+                
+                p {
+                    font-size: .8rem;
         }
 
         .ms_card-subtitle {
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,16 +19,16 @@ export default {
     <section id="cards-section">
         <div class="container bg-light py-5">
             <div class="cards-section-top bg-dark p-3 my-4">
-                <h3 class="text-light">Found 62 characters</h3>
+                <h3 class="text-light">{{ `Found ${store.characters.length} characters` }}</h3>
             </div>
 
             <div class="card-section-bottom">
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
-                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-5">
-                                <div class="col">
-                                    <AppCards />
+                            <div class="row g-4 row-cols-1 row-cols-md-2 row-cols-lg-5">
+                                <div class="col" v-for="character in store.characters" :key="store.characters.char_id">
+                                    <AppCards :character="character"/>
                                 </div>
                             </div>
                         </div>
@@ -41,8 +41,6 @@ export default {
 
 <style lang="scss" scoped>
 .cards-section-top{
-    width: 90%;
-    margin: 0 auto;
     h3{
         font-size: 1rem;
     }
