316122d163fb5ec10d60568b074f6375f8b1d560
BetterCallAle <dap22091998@gmail.com>
2022-11-11 15:41:04 +0100

    added 'form-select' class to select in AppSelector.vue

--- src/components/AppSelector.vue
+++ src/components/AppSelector.vue
@@ -14,10 +14,17 @@ export default {
 <template>
     <section id="option-selection">
         <div class="container">
-            <select name="category" id="select-category" v-model="store.optionValue" @change="$emit('optionChanged')">
+            
+            <select id="select-category"
+                    v-model="store.optionValue"
+                    @change="$emit('optionChanged')"
+                    class="form-select w-25"
+            >
+
                 <option value="">Select Category</option>
                 <option value="Breaking Bad">Breaking Bad</option>
                 <option value="Better Call Saul">Better Call Saul</option>
+
             </select>
         </div>
     </section>
